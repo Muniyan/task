32b1900924cac4cd9cffa91a5ce94651de665eed
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-28 18:02:21 +0530

    Updates

---
+++ precache-manifest.0a28558487a8e3e6a02e3410701a09ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ff098ff3d4097579dbb",
+    "url": "/static/js/main.f45b7b52.chunk.js"
+  },
+  {
+    "revision": "7993f64b3e9e13c644b9",
+    "url": "/static/js/2.6c40d801.chunk.js"
+  },
+  {
+    "revision": "7ff098ff3d4097579dbb",
+    "url": "/static/css/main.497deffc.chunk.css"
+  },
+  {
+    "revision": "82450d8cbf09b9c018b510f42d029365",
+    "url": "/index.html"
+  }
+];
